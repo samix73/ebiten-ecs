96cc01739b405bc86bb205e6d0a76aa80ceb9e0f
Saman Yousefisohi <samix.ys@gmail.com>
2025-09-13 22:34:57 +0200

    Add documentation comments for ID, System, BaseSystem, and World interfaces; enhance clarity on methods and responsibilities

--- id.go
+++ id.go
@@ -6,10 +6,13 @@ import (
 
 var nextID = atomic.Uint64{}
 
+// NextID generates and returns a new unique ID.
 func NextID() ID {
 	return ID(nextID.Add(1))
 }
 
+// ID represents a unique identifier for entities and systems within the ECS framework.
 type ID uint64
 
+// UndefinedID is a constant representing an undefined or invalid ID.
 const UndefinedID ID = 0
--- world.go
+++ world.go
@@ -4,6 +4,10 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// World is the interface that all game worlds must implement.
+// A world is responsible for managing the game state, including entities and systems.
+// Each world must provide methods for updating, drawing, and tearing down the world.
+// Additionally, a world must be initialized with a reference to the Game instance.
 type World interface {
 	Update() error
 	Draw(screen *ebiten.Image)
@@ -13,6 +17,7 @@ type World interface {
 	baseWorld() *BaseWorld // Force embedding BaseWorld
 }
 
+// BaseWorld provides a foundational implementation of the World interface.
 type BaseWorld struct {
 	systemManager *SystemManager
 	entityManager *EntityManager
@@ -22,6 +27,7 @@ func (bw *BaseWorld) baseWorld() *BaseWorld {
 	return bw
 }
 
+// NewBaseWorld creates a new BaseWorld with the given EntityManager and SystemManager.
 func NewBaseWorld(entityManager *EntityManager, systemManager *SystemManager) *BaseWorld {
 	return &BaseWorld{
 		entityManager: entityManager,
@@ -29,6 +35,8 @@ func NewBaseWorld(entityManager *EntityManager, systemManager *SystemManager) *B
 	}
 }
 
+// Update updates the world by updating all systems managed by the SystemManager.
+// If any system returns an error during its update, the process is halted and the error is returned.
 func (w *BaseWorld) Update() error {
 	if err := w.SystemManager().Update(); err != nil {
 		return err
@@ -37,18 +45,24 @@ func (w *BaseWorld) Update() error {
 	return nil
 }
 
+// Draw draws the world by calling the Draw method of all systems that implement the DrawableSystem interface.
 func (w *BaseWorld) Draw(screen *ebiten.Image) {
 	w.SystemManager().Draw(screen)
 }
 
+// SystemManager returns the SystemManager associated with the world.
 func (w *BaseWorld) SystemManager() *SystemManager {
 	return w.systemManager
 }
 
+// EntityManager returns the EntityManager associated with the world.
 func (w *BaseWorld) EntityManager() *EntityManager {
 	return w.entityManager
 }
 
+// Teardown tears down the world by calling the Teardown method of all systems that implement the Teardowner interface
+// and then tearing down the EntityManager.
 func (m *BaseWorld) Teardown() {
 	m.SystemManager().Teardown()
+	m.EntityManager().Teardown()
 }
